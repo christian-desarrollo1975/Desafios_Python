f7090c39c0c7f21c5a53309ed95166be41e96364
Christian <estudio.csaluzzo@gmail.com>
2024-11-19 19:51:16 -0300

    Reloj

---
+++ reloj.py
@@ -0,0 +1,33 @@
+"""import tkinter as tk
+import time "
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+    clock_label.after(1000, update_time)
+
+root = tk.Tk()
+root.title("Reloj Digital")
+
+clock_label = tk.Label(root, font=("Arial", 80), bg="black", fg="white")
+clock_label.pack(padx=20, pady=20)
+
+update_time()
+root.mainloop()"""
+
+import tkinter as tk
+import time
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+    clock_label.after(1000, update_time)
+
+root = tk.Tk()
+root.title("Reloj Digital")
+
+clock_label = tk.Label(root, font=("Arial", 80), bg="black", fg="green")
+clock_label.pack(padx=20, pady=20)
+
+update_time()
+root.mainloop()
